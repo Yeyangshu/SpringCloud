81e363d72d6fe485ad0c7ba9262d565f487452c9
Yeyangshu <1107733786@qq.com>
2020-08-07 00:18:41 +0800

    eureka

---
+++ src/test/java/com/yeyangshu/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeyangshu.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
