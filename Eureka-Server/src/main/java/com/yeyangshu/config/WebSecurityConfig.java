7b72cfdb865bd352e646e68df901bf3f17f8495c
Yeyangshu <1107733786@qq.com>
2020-09-15 22:28:23 +0800

    feat(spring cloud): security 简单使用

---
+++ Eureka-Server/src/main/java/com/yeyangshu/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.yeyangshu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ * 关闭security防止跨域攻击配置类
+ * @author yeyangshu
+ * @version 1.0
+ * @date 2020/9/15 22:26
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+        http.csrf().disable();
+        super.configure(http);
+    }
+
+}
